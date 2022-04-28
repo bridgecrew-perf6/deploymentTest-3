aef5b201b8c385798278c60d10921c69b34de75d
Sebastian <sebastian.juras@gmail.com>
2022-04-28 11:10:08 +0200

    Deployment

---
+++ precache-manifest.f55960b087a8d54308d47250c29b6026.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4240efcc2d4357109515cf52ab2cc833",
+    "url": "/index.html"
+  },
+  {
+    "revision": "788fcc333274efcf2671",
+    "url": "/static/css/2.148cc0e7.chunk.css"
+  },
+  {
+    "revision": "e90749db8dc14601cf56",
+    "url": "/static/css/main.b30806fd.chunk.css"
+  },
+  {
+    "revision": "788fcc333274efcf2671",
+    "url": "/static/js/2.3e8cbeff.chunk.js"
+  },
+  {
+    "revision": "f24b43d3afb04f6c9cea25be9ebdc537",
+    "url": "/static/js/2.3e8cbeff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e90749db8dc14601cf56",
+    "url": "/static/js/main.bd2a6be1.chunk.js"
+  },
+  {
+    "revision": "69e917c7c8a689acd8dd",
+    "url": "/static/js/runtime-main.8d014958.js"
+  },
+  {
+    "revision": "a3b2d28b4a86cd0f7a962808fd67342f",
+    "url": "/static/media/marker.a3b2d28b.png"
+  }
+]);
